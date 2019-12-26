548f26763b25d043c743c37ec8c902e49eabaaf5
quaint <qicong@ilarge.cn>
2019-12-26 11:32:23 +0800

    es 索引定义提交

---
+++ demo-es/src/main/java/com/quaint/demo/es/index/ItemIndex.java
@@ -0,0 +1,68 @@
+package com.quaint.demo.es.index;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import java.time.LocalDateTime;
+
+/**
+ * Desc: es 索引定义
+ * @author qi cong
+ * @date 2019-12-26 11:15
+ */
+@Document(indexName = "demo", type = "item")
+@Data
+public class ItemIndex {
+
+    /**
+     * 主键
+     */
+    @Id
+    private Long id;
+
+    /**
+     * 标题
+     */
+    @Field(type = FieldType.Text)
+    private String title;
+
+    /**
+     * 分类
+     */
+    @Field(type = FieldType.Auto)
+    private String category;
+
+    /**
+     * 评分
+     */
+    @Field(type = FieldType.Double, index = false)
+    private Double score;
+
+    /**
+     * 浏览量
+     */
+    @Field(type = FieldType.Long, index = false)
+    private Long pageViews;
+
+    /**
+     * 是否喜欢
+     */
+    @Field(type = FieldType.Boolean, index = false)
+    private Boolean like;
+
+    /**
+     * 图片地址
+     */
+    @Field(type = FieldType.Text, index = false)
+    private String images;
+
+    /**
+     * 创建日期
+     */
+    @Field(type = FieldType.Date, index = false)
+    private LocalDateTime createTime;
+
+}
