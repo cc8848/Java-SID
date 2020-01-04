14b4b5aa28410340d9a0873f43d127b4adbd0a52
quaint <qicong@ilarge.cn>
2020-01-04 17:26:24 +0800

    sql 修改

--- demo-elastic-search/src/main/resources/sql/demo_article.sql
+++ demo-elastic-search/src/main/resources/sql/demo_article.sql
@@ -2,6 +2,7 @@ CREATE TABLE `demo_article` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
   `title` varchar(255) DEFAULT NULL COMMENT '文章名称',
   `content` varchar(255) DEFAULT NULL COMMENT '内容',
+  `page_views` int(11) DEFAULT '0' COMMENT '浏览量',
   `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
