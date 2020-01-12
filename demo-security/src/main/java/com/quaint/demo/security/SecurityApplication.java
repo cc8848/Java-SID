08040e26f2ffc338649e6803cb98bf26e770b6c1
quaint <qicong@ilarge.cn>
2020-01-12 18:47:07 +0800

    spring boot security 15 分钟快速上手 小demo 需要继续学习

---
+++ demo-security/src/main/java/com/quaint/demo/security/SecurityApplication.java
@@ -0,0 +1,17 @@
+package com.quaint.demo.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author quaint
+ * @date 2020-01-07 17:46
+ */
+@SpringBootApplication
+public class SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class,args);
+    }
+
+}
---
+++ demo-security/src/main/java/com/quaint/demo/security/config/MvcConfig.java
@@ -0,0 +1,23 @@
+package com.quaint.demo.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * mvc 配置文件
+ * @author qi cong
+ * @date 2019-12-25 16:50
+ */
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+      registry.addViewController("/index").setViewName("index");
+      registry.addViewController("/").setViewName("index");
+      registry.addViewController("/hello").setViewName("hello");
+      registry.addViewController("/login").setViewName("login");
+    }
+
+}
